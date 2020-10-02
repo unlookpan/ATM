05f5a172e42d2dbfe9d9954eb05a85f9b2d97423
KKK. plan <guanglinw030@gmail.com>
2020-10-02 22:54:14 +0800

    Update mian.cpp

--- mian.cpp
+++ mian.cpp
@@ -71,33 +71,11 @@ void ATM::better() {
 	}
 }
 int main() {
-	//int i = 0;
-	/*int password_b;
-	int account_number_b;
-	int password_s;
-	int account_number_s;*/
-
 	ATM atm;
 	//输入账号和密码
-	/*cout << "请输入账号";
-	cin >> account_number_s;
-	cout << "请输入密码";
-	cin >> password_s;*/
 	atm.cin_account_number_s();
 	atm.cin_password_s();
 	atm.better();
-	/*for (i = 0; i < 2; i++) {
-		account_number_b = obj[i].get_account_number();
-		if (account_number_b == account_number_s) {
-			password_b = obj[i].get_password();
-			if (password_b == password_s)cout << "登录成功" << "\n";
-			else cout << "密码错误" << "\n";
-		}
-	}*/
-
-	/*for (i = 0; i < 2; i++) {
-		cout << obj[i].get_account_number() << "\n" << obj[i].get_password() << "\n";
-	}*/
 	return 0;
 }
 
