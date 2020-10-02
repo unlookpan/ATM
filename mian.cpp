34e681f2bb8c28dbc1e124553c02e530d5a6c5eb
KKK. plan <guanglinw030@gmail.com>
2020-10-02 22:22:52 +0800

    Update mian.cpp

--- mian.cpp
+++ mian.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//people obj[2] = {
-//		people(227,123),
-//		people(526,456)
-//};
-/*”√ªß¿‡*/
+/*Áî®Êà∑Á±ª*/
 class people {
 protected:
 	int account_number;
@@ -27,38 +23,7 @@ int people::get_account_number(void) {
 int people::get_password(void) {
 	return password;
 }
-/*ATM¿‡*/
-//class ATM {
-//protected:
-//
-//public:
-//	void better();
-//	void cin_account_number_s();
-//	void cin_password_s();
-//	void get_account_number_b(int i);
-//	void get_password_b(int i);
-//
-//private:
-//	int password_b;
-//	int account_number_b;
-//	int password_s;
-//	int account_number_s;
-//};
-//
-//void ATM::cin_account_number_s() {
-//	cout << "«Î ‰»Î’À∫≈";
-//	cin >> account_number_s;
-//}
-//void ATM::cin_password_s() {
-//	cout << "«Î ‰»Î√‹¬Î";
-//	cin >> password_s;
-//}
-//void ATM::get_account_number_b(int i) {
-//	account_number_b = obj[i].get_account_number();
-//}
-//void ATM::get_password_b(int i) {
-//
-//}
+
 int main() {
 	int i = 0;
 	int password_b;
@@ -69,18 +34,18 @@ int main() {
 		people(227,123),
 		people(526,456)
 	};
-	//ATM atm;
-	// ‰»Î’À∫≈∫Õ√‹¬Î
-	cout << "«Î ‰»Î’À∫≈";
+	
+	//ËæìÂÖ•Ë¥¶Âè∑ÂíåÂØÜÁ†Å
+	cout << "ËØ∑ËæìÂÖ•Ë¥¶Âè∑";
 	cin >> account_number_s;
-	cout << "«Î ‰»Î√‹¬Î";
+	cout << "ËØ∑ËæìÂÖ•ÂØÜÁ†Å";
 	cin >> password_s;
 	for (i = 0; i < 2; i++) {
 		account_number_b = obj[i].get_account_number();
 		if (account_number_b == account_number_s) {
 			password_b = obj[i].get_password();
-			if (password_b == password_s)cout << "µ«¬º≥…π¶" << "\n";
-			else cout << "√‹¬Î¥ÌŒÛ" << "\n";
+			if (password_b == password_s)cout << "ÁôªÂΩïÊàêÂäü" << "\n";
+			else cout << "ÂØÜÁ†ÅÈîôËØØ" << "\n";
 		}
 	}
 
