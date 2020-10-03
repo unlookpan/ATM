08e108dd8907d3c06aef073ab5145b4f3d67c7b8
KKK. plan <guanglinw030@gmail.com>
2020-10-03 18:08:09 +0800

    Update ATM.cpp

--- ATM/ATM.cpp
+++ ATM/ATM.cpp
@@ -3,25 +3,26 @@
 #include"people.h"
 using namespace std;
 
-extern int key;
+extern int key_1;
+extern int key_2;
 extern int card;
 extern people obj[2];
 
 void ATM::cin_account_number_s() {
-	cout << "ÇëÊäÈëÕËºÅ";
+	cout << "è¯·è¾“å…¥è´¦å·";
 	cin >> account_number_s;
 }
 
 void ATM::cin_password_s() {
-	cout << "ÇëÊäÈëÃÜÂë";
+	cout << "è¯·è¾“å…¥å¯†ç ";
 	cin >> password_s;
 }
 
-void ATM::get_account_number_b(int i) {                       //»ñÈ¡peopleÀàÖĞµÄaccount_number
+void ATM::get_account_number_b(int i) {                       //è·å–peopleç±»ä¸­çš„account_number
 	account_number_b = obj[i].get_account_number();
 }
 
-void ATM::get_password_b(int i) {                             //»ñÈ¡password
+void ATM::get_password_b(int i) {                             //è·å–password
 	password_b = obj[i].get_password();
 }
 
@@ -29,75 +30,124 @@ void ATM::get_balance_b(int i) {
 	balance_b = obj[i].get_balance();
 }
 
-void ATM::better() {                                          //µÇÂ¼ÑéÖ¤º¯Êı
+void ATM::get_in() {                                          //ç™»å½•éªŒè¯å‡½æ•°
 	int i;
-	for (i = 0; i < 2; i++) {
-		get_account_number_b(i);
-		if (account_number_b == account_number_s) {
-			get_password_b(i);
-			if (password_b == password_s) {
-				cout << "µÇÂ¼³É¹¦" << "\n";
-				ATM_function();
-				card = i;
-			}
-			else cout << "ÃÜÂë´íÎó" << "\n";
+	int t=1,r=1;
+	while (t) {
+		cout << "\n";
+		cin_account_number_s();
+		for (i = 0; i < 2; i++) {
+			get_account_number_b(i);
+			if (account_number_b == account_number_s) { 
+				t = 0; break; 
+			} 
+			
+		}
+		if(t)cout << "è´¦å·ä¸å­˜åœ¨\n";
+	}
+	while (r) {
+		cin_password_s();
+		get_password_b(i);
+		if (password_b == password_s) {
+			r = 0;
+		}
+		else {
+			cout << "å¯†ç é”™è¯¯\n";
+			cout << "æ‚¨å¯ä»¥ä¿®æ”¹å¯†ç \n";
+			change_password();
 		}
 	}
+	cout << "\nç™»å½•æˆåŠŸ\n" ;
+	card = i;
+	key_1 = 9;
 }
 
 void ATM::print_message() {
-	cout << "ÕËºÅ:" << obj[card].get_account_number() << "\n";
-	cout << "ÃÜÂë" << obj[card].get_password() << "\n";
-	cout << "Óà¶î" << obj[card].get_balance() << "\n";
-	cout << "\n" << "°´0ÍË¿¨";
-	cin >> key;
+	cout << "\n";
+	cout << "è´¦å·:" << obj[card].get_account_number() << "\n";
+	cout << "å¯†ç " << obj[card].get_password() << "\n";
+	cout << "ä½™é¢" << obj[card].get_balance() << "\n";
+	cout << "æŒ‰2å–æ¬¾\n";
+	cout << "æŒ‰0é€€å¡\n";
+	cin >> key_2;
 }
 
 void ATM::withdrawal_balance() {
-	cout << "È¡¿î½ğ¶î£º";
+	cout << "\n";
+	cout << "å–æ¬¾é‡‘é¢ï¼š";
 	cin >> withdrawal;
 	get_balance_b(card);
 	balance_b = balance_b - withdrawal;
+	obj[card].fu_balance(balance_b);
 	if (balance_b < 0) {
-		cout << "Óà¶î²»×ã\n";
+		cout << "ä½™é¢ä¸è¶³\n";
 	}
 	else {
-		cout << "È¡¿î½ğ¶î£º" << withdrawal << "\n";
-		cout << "Óà¶î£º" << balance_b << "\n";
+		cout << "ä½™é¢ï¼š" << balance_b << "\n";
 	}
-	cout << "°´0ÍË³ö";
-	cin >> key;
-
+	cout << "æŒ‰1æŸ¥è¯¢ä¿¡æ¯\n";
+	cout << "æŒ‰0é€€å‡º\n";
+	cin >> key_2;
 }
 
-//void ATM::change_password(people &x) {
-//	cout << "ĞŞ¸ÄµÄÃÜÂëÎª£º";
-//	cin >> cin_password;
-//	cout << x.password;
-//
-//	cout << "°´0ÍË³ö" << key;
-//}
+void ATM::change_password() {
+	int i;
+	int t = 1;
+	while (t) {
+		cout << "\n";
+		cin_account_number_s();
+		for (i = 0; i < 2; i++) {
+			get_account_number_b(i);
+			if (account_number_b == account_number_s) {
+				t = 0; break;
+			}
+		}
+		if(t)cout << "è´¦å·ä¸å­˜åœ¨\n";
+	}
+	cout << "ä¿®æ”¹çš„å¯†ç ä¸ºï¼š";
+	cin >> cin_password;
+	obj[i].fu_passsword(cin_password);
+	cout << "ä¿®æ”¹æˆåŠŸ\n";
+	get_in();
+}
 
 void ATM::ATM_function() {
-	cout << "°´1²é¿´ĞÅÏ¢\n";
-	cout << "°´2È¡¿î\n";
-	cout << "°´3ĞŞ¸ÄÃÜÂë\n";
-	cout << "°´0ÍË³ö\n";
-	cin >> key;
-	while (key) {
-		switch (key) {
-		case 0:
-			break;
-		case 1:
-			print_message();
-			break;
-		case 2:
-			withdrawal_balance();
-			break;
-		case 3:
-			//change_password(obj[card]);
-			break;
+	if (key_1==9) {
+		cout << "\n";
+		cout << "æŒ‰1æŸ¥çœ‹ä¿¡æ¯\n";
+		cout << "æŒ‰2å–æ¬¾\n";
+		cout << "æŒ‰0é€€å‡º\n";
+		cin >> key_2;
+		while (key_2) {
+			switch (key_2) {
+			case 0:
+				break;
+			case 1:
+				print_message();
+				break;
+			case 2:
+				withdrawal_balance();
+				break;
+			}
 		}
+		cout << "æ¬¢è¿ä¸‹æ¬¡ä½¿ç”¨";
 	}
-	cout << "»¶Ó­ÏÂ´ÎÊ¹ÓÃ";
+}
+
+void ATM::interface() {
+	cout << "æ¬¢è¿æ¥åˆ°ATMå–æ¬¾æœº\n";
+	cout << "æŒ‰1ç™»å½•\n";
+	cout << "æŒ‰2ä¿®æ”¹å¯†ç \n";
+	cout << "æŒ‰0é€€å‡º\n";
+	cin >> key_1;
+	switch (key_1) {
+	case 0:
+		break;
+	case 1:
+		get_in();
+		break;
+	case 2:
+		change_password();
+		break;
+	}
 }
