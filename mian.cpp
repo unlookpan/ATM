ee85393ad9bcc0337a04ad81a8caefe8feb21256
KKK. plan <guanglinw030@gmail.com>
2020-10-03 18:13:08 +0800

    Update mian.cpp

--- mian.cpp
+++ mian.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int card;//card为方便找到数组中的第几张卡
-int key = 0;//控制整个系统退出运行的参数
+int key_2 = 0;//控制整个系统退出运行的参数
+int key_1 = 0;
 
 int main() {
 	ATM atm;
-	atm.cin_account_number_s();
-	atm.cin_password_s();
-	atm.better();
+	atm.interface();//初始界面
+	atm.ATM_function();
 	return 0;
 }
 
